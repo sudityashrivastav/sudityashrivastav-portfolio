5cb6c2554bcc2201ef80e4a387f0de5834b5aa80
sudityashrivastav <sudityakumar1@gmail.com>
2023-11-22 18:23:54 +0530

    updated package file

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,20 +10,12 @@ module.exports = {
         "xl": "1050px"
       },
       colors: {
-
         bg_theme: "#06043E",
-        // Blue: #05EEFF
         primary: '#05EEFF',
         secondaryBlue: "#19174B",
         secondary: '#06043E',
         tertiary: '#FFFFFF80',
         suditya: '#06043E',
-
-
-        // primary: '#ec4755',
-        // secondary: '#a12c34',
-        // tertiary: '#99a0a3',
-        // suditya: '#ffffff78',
       },
     },
   },
